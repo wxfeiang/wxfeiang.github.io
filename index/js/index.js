4f8bf906c914e520e493a1d1d24b5642a54f7511
wxfeiang <wxfeiang@qq.com>
2018-02-06 14:27:50 +0800

    jsnew

--- index/js/index.js
+++ index/js/index.js
@@ -7,11 +7,13 @@ window.onscroll = function(){
 	//console.log(topScroll);
 	//console.log(h)
 	if(topScroll>innavTop){
-	innav.style.position = 'fixed';
-	innav.style.top = '0';
-	innav.style.left = '0';
-	innav.style.width = '100%';
-	innav.style.zIndex = "9999";
+	// innav.style.position = 'fixed';
+	// innav.style.top = '0';
+	// innav.style.left = '0';
+	// innav.style.width = '100%';
+	// innav.style.zIndex = "9999";
+    innav.style.cssText = "position:fixed;top:0; width :100%;z-index:999;";
+
 
 	//console.log(topScroll-h);
 	}
@@ -21,26 +23,8 @@ window.onscroll = function(){
 	
 	 }
 
-  /*baout me*/
- 
-
+  
  }
-/*点击按钮*/
-var navbtn = getId("navbtn");
-var navlist = getId("navlist");
-var onOff = true;;
-navbtn.onclick = function() {
-	if(onOff){
-		navlist.style.display = 'block';
-		onOff = false;
-	}else{
-		navlist.style.display = 'none';
-		onOff = true;
-	}
-
-	
-}
-
 /*aboutme*/
 
 	var abmel = getId("abmel");
@@ -55,17 +39,99 @@ navbtn.onclick = function() {
 	     <li><i class="fa fa-phone"></i><a href="tel:18309469751"> &nbsp18309469751</a></li>
 
 	     `;
+	     //console.log(code.length);
 	var coder = `
 	   <h3 class="right-title">关于我</h3>
 	   <p class="right-dec">在前端开发工作中，让我变得更有耐心，思维更活跃， 也培养了我独立思考和自主学习的能力，在压力和困难面前我有很好的分析能力和解决问题的能力。 我更是一位富有团队精神的人，能够相互协作和充分的沟通。谋求公司的<span>Web前端开发<span>职位。
 	   </p> 
 	  `;
-   var timer = setInterval(abauto,50);
+   var timer1 = setInterval(abauto,50);
    function abauto(){
-   	 abmel.innerHTML = code.substring(0,n);
-	 abmer.innerHTML = coder.substring(0,n);
-	   n++;
+   	if(n<=code.length ||n<=coder.length  ){
+   		 abmel.innerHTML = code.substring(0,n);
+	     abmer.innerHTML = coder.substring(0,n);
+	     n++;
+   	   }
+   	   else{
+   	   	  return false;
+   	   }
    }
+
+  // clearInterval(timer1);
+
+ /* app 下点击效果*/
+var navbtn = getId("navbtn");
+var navlist = getId("navlist");
+var onOff = true;;
+navbtn.onclick = function() {
+	if(onOff){
+		navlist.style.display = 'block';
+         navlist.onclick = function(){
+          navlist.style.display = 'none';
+
+         }
+
+		onOff = false;
+	}else{
+		navlist.style.display = 'none';
+		onOff = true;
+	}
+
+	
+}
+// 楼层跳转
+var nlarr = navlist.children;
+var flarr = document.getElementsByClassName("flarr");
+
+
+for (var i=0;i<nlarr.length; i++){
+  nlarr[i].index = i;
+  nlarr[i].onclick = function (){
+  
+  objto(flarr[this.index]);
+  
+  }
+}
+
+/*滚动*/
+function objto (obj){
+	var target = 0;  // 目标位置
+	var timer = null; 
+	var leader = 0;  // 滚动条初始外置
+
+	  target = obj.offsetTop;
+	  clearInterval(timer);
+	  timer =setInterval(function(){
+	  //console.log(leader) ;
+	    var speed = (target-leader)/10;  
+	    // 判断方向  
+	    speed = speed>0?Math.ceil(speed):Math.floor(speed);  
+	    //下一步到的位置  
+	    leader = leader+speed;  
+	    //移动  
+	    window.scrollTo(0,leader);  
+	    //判断是否到达，到达清除定时器  
+	    if(leader == target){  
+	        //console.log(1);  
+	        clearInterval(timer);  
+	    }  
+
+	  },30);
+
+    }
+// 首页按钮
+var spbtn = document.querySelectorAll(".ba-sbtn span");
+spbtn[0].onclick = function (){
+	objto(flarr[1]);
+}
+
+spbtn[1].onclick = function (){
+	objto(flarr[4]);
+}
+
+
+
+
    
 /*contact-me*/
 //  初始化数据库对象
@@ -118,26 +184,13 @@ function submitForm(e){
     //弹框
     function aotoNone(inB,inHtml){
       inalert.style.display = inB ;
-      inalertp.innerHTML = inHtml ;
+      inalertp.innerHTML = inHtml ; 
       setTimeout(onSure,2000);
     }
 
 
 }
-
-
-
-/*获取ID元素*/
-function getId(id){
-	return document.getElementById(id);
-}
-
-//时间对象处理
- function  toTwo(date) {
-   return  date < 10 ?  "0"+date :date;
- }
-
- /*数据存储  josn*/
+/*数据存储  josn*/
 function saveMessage (message,nowday,nowtime){
  messageRef. push({
         message : message,
@@ -145,16 +198,16 @@ function saveMessage (message,nowday,nowtime){
         nowtime : nowtime
      });
 }
-// 获取数据 
+// 服务器获取数据 
 messageRef.orderByKey().limitToLast(5).on('value',function (snapshot){
-	  // console.log(snapshot.key());
-	   //console.log(snapshot.val());
-	   // console.log("the previous key is",prev)
+	  // console.log(snapshot.key());  数据表
+	   //console.log(snapshot.val());  5调objact
+       //
 	var getarr= snapshot.val();
 	var getpu=[];
 	for(i in getarr){
 	   getpu.push({
-	    mg:getarr[i].message,
+	    mg: getarr[i].message,
 	    da:	getarr[i].nowday,
 	    dd:	getarr[i].nowtime
 	   })
@@ -178,3 +231,16 @@ messageRef.orderByKey().limitToLast(5).on('value',function (snapshot){
 
 	getId("newlist").innerHTML = str;
 });
+
+
+/*获取ID元素*/
+function getId(id){
+	return document.getElementById(id);
+}
+
+//时间对象处理
+ function  toTwo(date) {
+   return  date < 10 ?  "0"+date :date;
+ }
+
+ 
