028d31cd799fc8b762825f49bf6abcdd8309d86a
王鹏 <wangpeng@wangpengdeMacBook-Pro.local>
2022-01-25 22:18:48 +0800

    开始维护

--- lantu/js/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2017/3/17.
- */
-(function(){
-    var $more = $('.header .more');
-    $more.click(function(){
-        $(this).find('.more-hide').toggle();
-    });
-})();
